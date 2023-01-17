73879e8660c73c9e58deab4b63988f57a5897505
Lorrainem <moyolorraine98@gmail.com>
2023-01-17 17:37:04 +0200

    Created VillaDTO Model

---
+++ RaineVilla_VillaApi/Models/Dto/VillaDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RaineVilla_VillaApi.Models.Dto
+{
+    public class VillaDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
