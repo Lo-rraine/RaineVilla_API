0811d4fd2da97c299a93ff41c743f258645f1e8e
Lorrainem <moyolorraine98@gmail.com>
2023-01-17 17:37:20 +0200

    Created a temp store

---
+++ RaineVilla_VillaApi/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using RaineVilla_VillaApi.Models.Dto;
+
+namespace RaineVilla_VillaApi.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList= new List<VillaDTO>
+            {
+                new VillaDTO { Id=1, Name="Uvongo Beach"},
+                new VillaDTO {Id=2, Name="Shelly Beach"}
+            };
+    }
+}
