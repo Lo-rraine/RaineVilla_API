244dec4cadc2aaed0d4b55779301da8c97171a77
Lorrainem <moyolorraine98@gmail.com>
2023-01-20 14:18:42 +0200

    Add seeded data

--- RaineVilla_VillaApi/Data/ApplicationDbContext.cs
+++ RaineVilla_VillaApi/Data/ApplicationDbContext.cs
@@ -1,12 +1,80 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RaineVilla_VillaApi.Models;
+using System.Reflection.Emit;
+
 
 namespace RaineVilla_VillaApi.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options){}
-
         public DbSet <Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa()
+                {
+                    Id = 1,
+                    Name = "Royal Villa",
+                    Details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean arcu velit, faucibus vel turpis in, convallis condimentum massa",
+                    ImageUrl = "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                    Occupancy = 5,
+                    Rate = 200,
+                    SquareFeet = 500,
+                    Amenity = "Wifi",
+                    CreatedDate = DateTime.Now
+                },
+                new Villa()
+                {
+                    Id = 2,
+                    Name = "Royal Villa",
+                    Details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean arcu velit, faucibus vel turpis in, convallis condimentum massa",
+                    ImageUrl = "https://images.unsplash.com/photo-1596394516093-501ba68a0ba6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHJlc29ydHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+                    Occupancy = 5,
+                    Rate = 200,
+                    SquareFeet = 500,
+                    Amenity = "Wifi",
+                    CreatedDate = DateTime.Now
+                },
+                 new Villa()
+                 {
+                     Id = 3,
+                     Name = "Uvongo Villa",
+                     Details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean arcu velit, faucibus vel turpis in, convallis condimentum massa",
+                     ImageUrl = "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                     Occupancy = 5,
+                     Rate = 200,
+                     SquareFeet = 500,
+                     Amenity = "Wifi",
+                     CreatedDate = DateTime.Now
+                 }, 
+                 new Villa()
+                 {
+                     Id = 4,
+                     Name = "Shelly Villa",
+                     Details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean arcu velit, faucibus vel turpis in, convallis condimentum massa",
+                     ImageUrl = "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                     Occupancy = 5,
+                     Rate = 200,
+                     SquareFeet = 500,
+                     Amenity = "Wifi",
+                     CreatedDate = DateTime.Now
+                 }, 
+                 new Villa()
+                 {
+                     Id = 5,
+                     Name = "Manaba Villa",
+                     Details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean arcu velit, faucibus vel turpis in, convallis condimentum massa",
+                     ImageUrl = "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                     Occupancy = 5,
+                     Rate = 200,
+                     SquareFeet = 500,
+                     Amenity = "Wifi",
+                     CreatedDate = DateTime.Now
+                 });
+
+
+        }
     }
 }
