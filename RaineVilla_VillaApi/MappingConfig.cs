352006becd75fba8fdc567b90400d54c6c01cfc0
Lorrainem <moyolorraine98@gmail.com>
2023-01-31 19:55:32 +0200

    Automapper

---
+++ RaineVilla_VillaApi/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using RaineVilla_VillaApi.Models;
+using RaineVilla_VillaApi.Models.Dto;
+
+namespace RaineVilla_VillaApi
+{
+    public class MappingConfig : Profile 
+    {
+        public MappingConfig()
+        {
+            CreateMap<Villa, VillaDTO>();
+            CreateMap<VillaDTO, Villa>();
+
+            CreateMap<Villa, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+        }
+    }
+}
--- RaineVilla_VillaApi/Program.cs
+++ RaineVilla_VillaApi/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using RaineVilla_VillaApi;
 using RaineVilla_VillaApi.Data;
 using RaineVilla_VillaApi.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddDbContext<ApplicationDbContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
