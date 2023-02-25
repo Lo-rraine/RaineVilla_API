5007207624594d5469965703cb2071eeb0692efa
Lorrainem <moyolorraine98@gmail.com>
2023-02-25 15:07:28 +0200

    Created Mapping config for DTO mappings

---
+++ RaineVilla_Web/MappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using RaineVilla_Web.Models.Dto;
+
+namespace RaineVilla_Web
+{
+    public class MappingConfig : Profile 
+    {
+        public MappingConfig()
+        {
+            CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+            CreateMap<Villa, VillaCreateDTO>().ReverseMap();
+            CreateMap<Villa, VillaUpdateDTO>().ReverseMap();
+
+            CreateMap<VillaNumber, VillaNumberDTO>().ReverseMap();
+            CreateMap<VillaNumber, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumber, VillaNumberUpdateDTO>().ReverseMap();
+        }
+    }
+}
